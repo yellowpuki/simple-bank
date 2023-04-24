97ea314b0c082ef13a69fc4bada365915b474738
Pavel Timokhovich <yellowpukipost@gmail.com>
2023-04-24 21:24:52 +0300

    Add HashedPassword func

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,9 +10,12 @@ import (
 )
 
 func createRandomUser(t *testing.T) User {
+	hashPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:     util.RandomOwner(),
-		HashPassword: "secret",
+		HashPassword: hashPassword,
 		FullName:     util.RandomOwner(),
 		Email:        util.RandomEmail(),
 	}
